e2a77562dc1972a29efd71b4f02ee64196ac5973
Jiyu Tian <tjyiiuan@gmail.com>
2018-10-04 12:48:39 -0400

    Basic framework

--- EECE5639-Computer-Vision/Project-1/libs/misc.py
+++ EECE5639-Computer-Vision/Project-1/libs/misc.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+import glob
 import numpy as np
+import matplotlib.image as mpimg
 
 
 def EST_NOISE(images):
@@ -9,4 +11,54 @@ def EST_NOISE(images):
     m_sigma = np.sqrt(sum((images - m_e_bar)**2) / (num - 1))
 
     return m_sigma
+
+
+def Load_Images(path, imgtype="*.jpg"):
+    """Load frame images from folder.
+
+    Parameters
+    ----------
+    path: string
+        Image path
+    imgtype: string
+        Type of images
+    """
+    loadpath = f"{path}{imgtype}"
+    all_img_path = glob.glob(loadpath)
+    img_num = len(all_img_path)
+    all_img = [0] * img_num
+    for i in np.arange(img_num):
+        one_img_path = all_img_path[i]
+        all_img[i] = mpimg.imread(one_img_path)
+    all_img = np.array(all_img)
+    
+    return all_img
+
+
+def Gen_Gaussian_Mask(dim, n, sigma):
+    """Generate 1D or 2D Gaussian filter.
+    
+    Parameters
+    ----------
+    dim: int
+        Dimension of filter
+    n: int
+        Size
+    sigma: float
+        Standard deviation
+    """
+    ovrlay = int(n / 2)
+    inds = np.arange(-ovrlay, ovrlay + 1)
+    gaussian_1d = np.exp(-inds**2/(2 * sigma**2))
+    mask = gaussian_1d /sum(gaussian_1d).reshape((-1, 1))
+    
+    if n == 2:
+        mask = gaussian_1d * gaussian_1d.T
+        
+    return mask
+
+
+
+
+
 
--- EECE5639-Computer-Vision/Project-1/project1.py
+++ EECE5639-Computer-Vision/Project-1/project1.py
@@ -9,16 +9,20 @@ plt.rcParams['figure.figsize'] = 15, 6
 #plt.rcParams['figure.dpi'] = 300
 
 from libs.FilterApp import FilterApp
+from libs.misc import Load_Images, Gen_Gaussian_Mask
 
+
+MASK_LIST = ["Box", "Gaussian"]
 #%%   
 class MotionDetector(object):
     
-    def __init__(self):
+    def __init__(self, frames):
         self.filapp = FilterApp()
-    
+        self.raw_frames = frames
+
     @staticmethod
     def rgb2gray(img):
-        """Convert RGB image to gray scale."""
+        """Convert a RGB image to gray scale."""
         if img.shape[-1] == 3:
             tmpimg = 0.299 * img[:, :, 0] + 0.587 * img[:, :, 1] + 0.114 * img[:, :, 2]
         else:
@@ -26,36 +30,63 @@ class MotionDetector(object):
 
         return tmpimg
         
-    def Load_Images(self, path, imgtype="*.jpg"):
-        """Load frame images from folder.
+    def Convert_Gray(self):
+        """Convert raw frames into gray scale."""
+        tmp_frames = self.raw_frames
+        img_num = tmp_frames.shape[0]
+        gray_frames = [0] * img_num
+        for i in np.arange(img_num):
+            current_frame = tmp_frames[i]
+            gray_frames[i] = self.rgb2gray(current_frame)
 
+        gray_frames = np.array(gray_frames)
+        self.gray_frames = gray_frames
+
+        return self
+    
+    def Spatial_Smooth(self, mask="None", **kwargs):
+        """Applying a 2D spatial smoothing fi
+lter to the frames 
+        before applying the temporal derivative fi
+lter.
+        
         Parameters
         ----------
-        path: string
-            Image path
-        imgtype: string
-            Type of images
+        mask: string, default 'None'
+            Spatial smoothing fi
+lter to apply
         """
-        loadpath = f"{path}{imgtype}"
-        all_img_path = glob.glob(loadpath)
-        img_num = len(all_img_path)
-        all_img = [0] * img_num
-        for i in np.arange(img_num):
-            one_img_path = all_img_path[i]
-            rgbimg = mpimg.imread(one_img_path)
-            all_img[i] = self.rgb2gray(rgbimg)
+        
+        spatial_filter = None
+        if mask in MASK_LIST:
+            pass
 
-        all_img = np.array(all_img)
-        self.AllImage = all_img
-
-        return all_img
-            
+        return self
+    
+    def Temporal_Derive(self, operator, **kwargs):
+        """apply a 1-D differential operator at each pixel to 
+        compute a temporal derivative.
+        
+        Parameters
+        ----------
+        operator: string
+            Differential operator to apply
+        """
+        
+        return self
+    
+    def Show_Result(self):
+        pass
 #%%
 if __name__ == "__main__":
-    Mdetector = MotionDetector()
-    Mdetector.Load_Images("./EnterExitCrossingPaths2cor/")
-    b = Mdetector.AllImage
-
+    frame_path = "./EnterExitCrossingPaths2cor/"
+    
+    frames = Load_Images(frame_path)
+    Mdetector = MotionDetector(frames)
+    Mdetector.Convert_Gray()
+    Mdetector.Spatial_Smooth()
+    Mdetector.Temporal_Derive()
+    Mdetector.Show_Result()
 
 
 
